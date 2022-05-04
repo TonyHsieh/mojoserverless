54e4e0dcfcda727eabeb24e016dbc2d698097f92
Tony Hsieh <hsieh2888-github@yahoo.com>
2022-05-03 21:44:32 -0700

    Adding the Sprout logic.

---
+++ sproutMojo.js
@@ -0,0 +1,113 @@
+'use strict'
+const AWS = require('aws-sdk')
+const ethers = require('ethers')
+
+module.exports.getMojo = async (event) => {
+
+  // target of the UPDATE (put)
+  const uuid = event.pathParameters.id;
+
+  let statusCodeVal = 200;
+  let bodyVal = "";
+
+  // Look up the target Mojo  
+  const scanParams = {
+    TableName: process.env.DYNAMODB_MOJO_TABLE,
+    Key: {
+      uuid: event.pathParameters.id,
+    },
+  };
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const result = await dynamodb.get(scanParams).promise();
+
+
+  //This is the JSON input that is needed in the body
+  //EXAMPLE:
+  //		{
+  //      	  "signerAddr": "0xaaaaaaaaaaa",
+  //              "signature": "0xfffffffff",
+  //      	  "signedMessage": "0xdddddddddd",
+  //      	  "domain": {
+  //      	    "name": "Test Domain",
+  //      	    "version": "0.x.0",
+  //      	    "chainId": 1,
+  //      	    "verifyingContract": "0xccccccccccc"
+  //      	  },
+  //      	  "types": {
+  //      	    "Sale": [
+  //      	      {
+  //      	        "name": "tokenId",
+  //      	        "type": "uint256"
+  //      	      },
+  //      	      {
+  //      	        "name": "price",
+  //      	        "type": "uint256"
+  //      	      }
+  //      	    ]
+  //      	  },
+  //      	  "value": {
+  //      	    "tokenId": 9999,
+  //      	    "price": 101
+  //      	  }
+  //             }
+  const body = JSON.parse(Buffer.from(event.body, 'base64').toString())
+
+  console.log("1 =====================");
+  console.log(JSON.stringify(body));
+  console.log("2 =====================");
+  console.log(JSON.stringify(result.Item.isSprouted));
+  console.log("3 =====================");
+  // check if isSprouted already true or non-zero ( timestamp )
+  if ("isSprouted" in result.Item) {
+    // if true - then response is 200 OK. 
+    statusCodeVal = 200;
+  } else {
+    //if false - then 
+   
+    // do the signature check logic
+    const recoveredAddr = ethers.utils.verifyTypedData(body.domain, body.types, body.value, body.signature)
+    const isSignerMatching = !!(recoveredAddr === body.signerAddr)
+
+    console.log ("recoveredAddr = " + recoveredAddr);
+    console.log ("signerAddr = " + body.signerAddr);
+
+    if (!isSignerMatching) {
+
+      //if it not verified then response is 400 Bad Request with 
+      // signature check failed message.  
+      statusCodeVal = 400;
+      bodyVal = {
+        type: "/errors/signature-verification-fail",
+        title: "Signature verification fail.",
+        status: 400,
+        detail: "Authentication failed due to signature verification fail.",
+        instance: "/sproutMojo/"+ uuid,
+      }    
+    } else {
+      //if verified then 
+    
+      //add isSprouted with timeStamp (Date.now())
+      result.Item.isSprouted = Date.now();
+    
+    //update the result-body to DynamoDB
+      const putParams = {
+        TableName: process.env.DYNAMODB_MOJO_TABLE,
+        Item: JSON.stringify(result.Item), 
+      }
+
+      await dynamoDb.put(putParams).promise();
+
+      //response is 200 OK
+      statusCodeVal = 200;
+    } 
+
+
+    console.log(JSON.stringify(result.Item));
+
+    return {
+      statusCode: statusCodeVal,
+      body: JSON.stringify(bodyVal),
+    };
+  }
+}
