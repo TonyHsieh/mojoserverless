e6ac2c4642253c4f13cd0d6abc2db0f1f8ef565a
Tony Hsieh <hsieh2888-github@yahoo.com>
2022-06-04 22:50:08 -0700

    getMojoSeed will always return data.  sproutMojoSeed will also check the Mojo Contract for existance and will sprout/reveal if that is true.  In addition to the sprouter contract ownership.

--- mojoSeed.js
+++ mojoSeed.js
@@ -226,6 +226,7 @@ module.exports.getMojoSeed = async (event) => {
   console.log("0.1 =====================");
   console.log(JSON.stringify(returnSeedMetaData));
 
+  /*
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const result = await dynamodb.get(scanParams).promise();
 
@@ -246,10 +247,11 @@ module.exports.getMojoSeed = async (event) => {
       bodyVal = returnSeedMetaData; 
     }
   }
+  */
   
   return {
     statusCode: statusCodeVal,
-    body: JSON.stringify(bodyVal),
+    body: JSON.stringify(returnSeedMetaData),
   };
 }
 
@@ -349,9 +351,9 @@ module.exports.sproutMojoSeed = async (event) => {
 
   // ------------ 
   // target of the UPDATE (put)
-  const uuid = event.pathParameters.id;
+  //const uuid = event.pathParameters.id;
   // TEST - This for testing locally
-  //const uuid = "3"; // must be a string!
+  const uuid = "3"; // must be a string!
   
   // The default return response
   const returnSeedMetaData = {
@@ -387,8 +389,8 @@ module.exports.sproutMojoSeed = async (event) => {
     // if NOT isSprouted and currentTime < nextCheckTime then do nothing and return (unrevealed) Seed Movie
     // if NOT isSprouted and currentTime > nextCheckTime then
     //          update the nextCheckTime to Date.now() plus 15 seconds (so you can't overcheck the contract)
-    //          do a check (which costs money) for the SeedPlanted Contract 
-    //          if isPlanted then update NFT to be isSprouted = Date.now()
+    //          do a check (which costs money) for the SeedPlanted Contract and the MojoExists Contract
+    //          if isPlanted || isMojoExists then update NFT to be isSprouted = Date.now()
     //          write out the value back to DynamoDB
     //
     if (result.Item.isSprouted) {
@@ -404,9 +406,11 @@ module.exports.sproutMojoSeed = async (event) => {
       // if not sprouted and enough time has elapse then check the sprouterContract. 
 
       const isPlanted = await callContractIsSeedPlanted(uuid); 
+      const isMojoExists = await callMojoContract(uuid);
 
       console.log("3 =====================");
       console.log("isPlanted = " + isPlanted);
+      console.log("isMojoExists = " + isMojoExists);
 
       // update nextCheckTime to Date.now() + (15 second * 1000 ms)
       result.Item.nextCheckTime = Date.now() + (15 * 1000);
@@ -414,7 +418,7 @@ module.exports.sproutMojoSeed = async (event) => {
       statusCodeVal = 200;
 
       // ifPlanted but not Sprouted then write new isSprouted to be true.
-      if (isPlanted && !result.Item.isSprouted) {
+      if ((isPlanted || isMojoExists) && !result.Item.isSprouted) {
         //Add the information here.
         result.Item.isSprouted = Date.now();
         bodyVal = { message: "Sprouting Complete"}; 
@@ -490,6 +494,33 @@ module.exports.sproutMojoSeed = async (event) => {
 
     // if not sprouted and enough time has elapse then check the sprouterContract. 
     return (sprouterContract.isSeedPlanted(_id));
+  }
+
+  // --------
+  async function callMojoContract(_id) {
+
+    console.log("**** Entering callMojoContract");
+
+    // Prepare for callins the Contract's isSeedPlanted
+    const provider = new ethers.providers.JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/'+ apiKey);
+
+    console.log("process.env.AWS_LAMBDA_FUNCTION_NAME: ", process.env.AWS_LAMBDA_FUNCTION_NAME);
+    if (process.env.AWS_LAMBDA_FUNCTION_NAME.indexOf("prod") != -1) {
+      console.log("   Choose PROD mojo contract!");
+      const mojoContractAddress = "0x34bff0c8eC197D72c4cb95Ee5a8Be9644FED5022"; // PRODUCTION
+    } else {
+      console.log("   Choose DEV mojo contract!");
+      const mojoContractAddress = "0x34bff0c8eC197D72c4cb95Ee5a8Be9644FED5022"; // DEV
+    }
+
+    const mojoContract = new Contract(
+      mojoContractAddress,
+      [ "function exists(uint256 tokenId) external returns (bool)"],
+      provider
+    );
+
+    // if not sprouted and enough time has elapse then check the mojoContract. 
+    return (mojoContract.exists(_id));
     
   } 
 }
